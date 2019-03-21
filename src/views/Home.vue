050f42fa9dc092b08e85151788fe576b8346d6f6
abhaisasidharan <abhaisasidharan@gmail.com>
2019-03-21 22:13:05 +0530

    Google analytics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to My Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to My Vue.js + JavaScript App"/>
   </div>
 </template>
 
